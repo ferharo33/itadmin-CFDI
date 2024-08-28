fc6b5aee01e6a53ab51545db938b5b897d14862e
Fernando Rivero <frivero@itadmin.com.mx>
2024-08-28 05:37:40 -0500

    [FIX] ddecimal in local tax

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '14.33',
+    'version': '14.34',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
