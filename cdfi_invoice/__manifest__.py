e25797e731b48127e334c9c4a64d1f31bde9de67
Fernando Rivero <frivero@x1carbon>
2025-02-14 11:48:52 -0500

    [FIX] do not copy fields

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '18.08',
+    'version': '18.10',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
