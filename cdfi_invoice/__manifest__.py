cd57abfe886806a2ac88ff73d81cafed4c3c4f0c
Fernando Rivero <frivero@x1carbon>
2025-04-22 13:21:32 -0400

    [FIX] discount compute

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '16.39',
+    'version': '16.40',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
