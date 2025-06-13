f2da07be4d9f1c32c269e8a398f77f0839e97384
Fernando Rivero <frivero@x1carbon>
2025-06-13 16:12:37 -0400

    [FIX] equivalenciadr

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '16.41',
+    'version': '16.42',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
